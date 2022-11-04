5cd507388a4b1d1e5e8b32f92808f78d809e4d14
jowander <74373353+jowander@users.noreply.github.com>
2022-11-04 17:43:46 +0100

    test ongoing

--- cypress/e2e/0-my-test/login.cy.js
+++ cypress/e2e/0-my-test/login.cy.js
@@ -1,9 +1,20 @@
+const speed = 500;
+
 describe("Login", () => {
   it("can login as valid user", () => {
     cy.visit("http://127.0.0.1:5500/");
-    cy.get("#register-form").click("button.btn.btn-outline-success");
-    cy.get("input#loginEmail").type("jowander_@noroff.no", { delay: 200 });
-    cy.get("input#password").type("12345678{enter}", { delay: 200 });
-    cy.url().should("include", "/index");
+    cy.get("button.btn.btn-outline-success").last().click({ delay: speed });
+    cy.get("input#loginEmail").type(
+      "jowander_@noroff.no",
+      { force: true },
+      { delay: speed }
+    );
+    cy.get("input#loginPassword").type(
+      "12345678{enter}",
+      { force: true },
+      { delay: speed }
+    );
+
+    //cy.url().should("include", "/index");
   });
 });
